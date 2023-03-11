d6934d008c776e72a052d32437fd953483b8594a
yashasan <104819731+yashasan@users.noreply.github.com>
2023-03-11 20:48:48 +0530

    Add files via upload

---
+++ Chapter4Sol.java
@@ -0,0 +1,47 @@
+public class Chapter4Sol {
+    public static void main(String[] args) {
+        // switch statements
+      /*  int age =70;
+        switch (age){
+            case 18 :
+                System.out.println("you are adult");
+                break;
+            case 23:
+                System.out.println("you are joining a job");
+                break;
+            case 60:
+                System.out.println("you are retiring");
+                break;
+            default:
+                System.out.println("enjoy");
+        }*/
+
+        // income tax
+
+     /*   float income = 5;
+        float tax =0;
+        if(income<=2.5){
+            System.out.println("no tax should be paid");
+        }
+        if(income>2.5 && income<=5){
+            tax=0.05f*(income -2.5f);
+            System.out.println("tax is"+" "+ tax );
+        }
+        if(income>5 && income<=10){
+            tax=tax+0.05f*(5.0f -2.5f);
+            tax=tax+0.2f*(income-5.0f);
+            System.out.println("tax is"+" "+ tax );
+        }
+        if(income>10){
+            tax = tax+0.05f*(5.0f-2.5f);
+            tax=tax+0.2f*(10.0f-5.0f);
+            tax=tax+0.3f*(income-10.0f);
+            System.out.println("tax is"+" "+ tax );
+        }
+*/
+
+
+
+
+    }
+}
