863f1d408671cc66247f9bd1b7ef6cd9451412ed
yashasan <104819731+yashasan@users.noreply.github.com>
2023-03-11 20:52:48 +0530

    Add files via upload
    5sol added

---
+++ Chapter5Sol.java
@@ -0,0 +1,85 @@
+public class Chapter5Sol {
+    public static void main(String[] args) {
+      /*  int i=0;
+        while (i<5){
+            System.out.println(i);
+            i++;
+            if(i==2){
+                System.out.println("breaking the statement");
+                break;
+            }
+
+              */          /*   int k=0;
+          for(int i=0;i<10;i++){
+
+              if(i==2){
+                  continue;
+              }System.out.println(i);
+             }*/
+
+
+        /* for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+
+      /* int n=0;
+        int k=0;
+        while(n<=5){
+            k=k+n;
+            n++;
+
+        }
+        System.out.println(k);*/
+
+       /*  int n=13;
+        for(int i=1;i<=10;i++){
+            System.out.println(n*i);
+        }*/
+
+       /* int n=10;
+        for(int i=10;i>=1;i--){
+            System.out.println(n*i);
+
+        }*/
+
+      /*  int n=10; int res=1;
+        for(int i=n;i>=1;i--){
+            res=res*i;
+
+
+        }System.out.println(res);*/
+
+
+
+      /*  int n=05; int res=1;
+        while(n>0){
+            res=res*n;
+            n--;
+        }
+        System.out.println(res);*/
+
+       /* int res=0; int n=8;
+        for(int i=1;i<=10;i++){
+            res=res+(n*i);
+
+
+        }System.out.println(res);*/
+
+
+       /* int n=5; int res=0;
+        for(int i=n;i>=1;i--){
+            res=res+i;
+        }
+        System.out.println(res);*/
+
+
+
+
+
+
+
+    }
+}
